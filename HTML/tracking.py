f53d787f26a692c88c85276a543b98af67872024
Z4K201909 <124842015+Z4K201909@users.noreply.github.com>
2023-11-15 17:33:41 -0600

    Update tracking.py

--- HTML/tracking.py
+++ HTML/tracking.py
@@ -2,14 +2,24 @@ import mysql.connector as mysql
 from bottle import route, run, request, response, app
 import json
 
-# Assuming dbConnector contains the database connection details
-from dbConnector import db_connection, cursor
+# Database connection details
+HOST = "34.68.154.206"
+DATABASE = "Post_Office_Schema"
+USER = "root"
+PASSWORD = "umapuma"
+
+# Establishing the connection
+db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+print("Connected to:", db_connection.get_server_info())
+
+# Creating a cursor object using the cursor() method
+cursor = db_connection.cursor(buffered=True)
 
 @app.route('/tracking-page.py', method='GET')
 def track_package():
     tracking_number = request.query.tracking_number
     if tracking_number:
-        query = "SELECT tracking_number FROM PACKAGE"
+        query = "SELECT status FROM PACKAGE WHERE tracking_number = %s"
         cursor.execute(query, (tracking_number,))
         status = cursor.fetchone()
 
@@ -22,6 +32,7 @@ def track_package():
 
     response.content_type = 'application/json'
     return json.dumps({'status': 'invalid'})
+
 
 # Replace HOST and PORT with your server's configuration
 # run(app, host='34.68.154.206', port=8080)
