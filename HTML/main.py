e057d3e6b4b4e5aa73f9f61b1d0ed2058e5c6304
zzclark17 <zzclark17@gmail.com>
2023-11-15 12:47:13 -0600

    made main.py continuous with while True

--- HTML/main.py
+++ HTML/main.py
@@ -3,9 +3,10 @@ from bottle import run, route, static_file
 from dbConnector import *
 from triggertest import *
 
-@route('/')
-def homePage():
-    return static_file('index.html', root='.')
+while True:
+    @route('/')
+    def homePage():
+        return static_file('index.html', root='.')
 
-if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080, debug=True)
+    if __name__ == '__main__':
+        run(host='0.0.0.0', port=8080, debug=True)
