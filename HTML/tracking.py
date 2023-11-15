46c19d36eabfdec472869bfbc887d11119e69686
Z4K201909 <124842015+Z4K201909@users.noreply.github.com>
2023-11-15 17:03:19 -0600

    Update tracking.py

--- HTML/tracking.py
+++ HTML/tracking.py
@@ -24,4 +24,4 @@ def track_package():
     return json.dumps({'status': 'invalid'})
 
 # Replace HOST and PORT with your server's configuration
-run(app, host='localhost', port=8080)
+run(app, host='34.68.154.206', port=8080)
