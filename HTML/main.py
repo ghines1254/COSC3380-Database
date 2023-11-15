010e39f5f27f6f12290d044b6dee73e261f75ed7
Gwyneth Hines <ghines1254@gmail.com>
2023-11-15 15:41:40 -0600

    plssss

--- HTML/main.py
+++ HTML/main.py
@@ -9,4 +9,4 @@ def homePage():
     return static_file('index.html', root='.')
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080, debug=True)
+    run(app, host='0.0.0.0', port=8080, debug=True)
