388280bb4b4d8a8bc7adb3e5040485e9cb03a641
zzclark17 <zzclark17@gmail.com>
2023-11-15 13:24:46 -0600

    edited main.py

--- HTML/main.py
+++ HTML/main.py
@@ -3,10 +3,10 @@ from bottle import run, route, static_file
 from dbConnector import *
 from triggertest import *
 
-while True:
-    @route('/')
-    def homePage():
-        return static_file('index.html', root='.')
 
-    if __name__ == '__main__':
-        run(host='0.0.0.0', port=8080, debug=True)
+@route('/')
+def homePage():
+    return static_file('index.html', root='.')
+
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=8080, debug=True)
