9a7a6838a0da4de967031f4683ec2f3d21dc3524
valjester <94697099+valjester@users.noreply.github.com>
2023-11-16 00:01:01 -0600

    Add files via upload

---
+++ HTML/phpfunctions.php
@@ -0,0 +1,23 @@
+<?php
+include("connection.php");
+
+function checklogin($con)
+    {
+        if(isset($_SESSION['userid']))
+        {
+            $id = $_SESSION['userid'];
+            $query = "SELECT W.UserId FROM WEBUSERS AS W WHERE W.UserId = '$id' limit 1";
+            $result = mysqli_query($con, $query);
+
+            if($result && mysqli_num_rows > 0)
+            {
+                $userdata = mysqli_fetch_assoc($result);
+                return $userdata;
+            }
+        }
+
+
+        header("Location: login-page.php");
+        die;
+        
+    }
