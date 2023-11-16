ccd82d7c61accbdef93c898fdc0b2989836008d5
Z4K201909 <124842015+Z4K201909@users.noreply.github.com>
2023-11-16 03:54:40 -0600

    Update employee-delivery-script.php

--- HTML/employee-delivery-script.php
+++ HTML/employee-delivery-script.php
@@ -13,40 +13,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Handle POST request for status update
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $packageId = $_POST['packageId'];
-    $status = $_POST['status'];
+    $newStatus = $_POST['newStatus'];
 
-    $stmt = $conn->prepare("UPDATE PACKAGE SET status = ? WHERE tracking_number = ?");
-    $stmt->bind_param("ss", $status, $packageId);
+    // Prepare and execute the update query
+    $stmt = $conn->prepare("UPDATE PACKAGE SET status = ? WHERE package_id = ?");
+    $stmt->bind_param("ss", $newStatus, $packageId);
     $stmt->execute();
 
     if ($stmt->affected_rows > 0) {
-        echo "Status updated successfully";
+        echo "Package status updated successfully.";
     } else {
-        echo "Error or no change in status";
-    }
-
-    $stmt->close();
-    $conn->close();
-    exit;
-}
-
-// Handle GET request for fetching status
-if (isset($_GET['tracking_number'])) {
-    $trackingNumber = $_GET['tracking_number'];
-
-    $stmt = $conn->prepare("SELECT status FROM PACKAGE WHERE tracking_number = ?");
-    $stmt->bind_param("s", $trackingNumber);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        echo json_encode(array('status' => $row['status']));
-    } else {
-        echo json_encode(array('status' => 'not_found'));
+        echo "Error updating package status or no changes made.";
     }
 
     $stmt->close();
