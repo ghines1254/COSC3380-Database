5aba00f9927b256a1b2a5c6d373ae7524d92c6f8
ghines1254 <ghines1254@gmail.com>
2023-11-15 23:16:08 -0600

    fdsfd

--- HTML/signupGrab.php
+++ HTML/signupGrab.php
@@ -1,6 +1,12 @@
 <?php
     if($_SERVER["REQUEST_METHOD"] == "POST")
     {
+        function get_unique_id() {
+            $uniqueRandomNumber = random_int(1000000000, 9999999999) . time();
+            return $uniqueRandomNumber;
+        }
+
+
         $customerID = get_unique_id();
         $firstName = $_POST['firstname'];
         $lastName = $_POST['lastname'];
@@ -14,15 +20,11 @@
         $phoneNum = $_POST['phone1'];
         $password = $_POST['password'];
 
+        echo $firstName . ' ' . $lastName;
 
-        function get_unique_id() {
-            $uniqueRandomNumber = random_int(1000000000, 9999999999) . time();
-            return $uniqueRandomNumber;
-        }
 
     }
     else{
         header("Location: ../sign-up-page.php");
     }
 
-    echo $firstName . ' ' . $lastName;
