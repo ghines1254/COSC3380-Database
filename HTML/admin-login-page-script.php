dc19a93ac46090c4b57fcb3970f4735dc196bb36
Z4K201909 <124842015+Z4K201909@users.noreply.github.com>
2023-11-16 16:53:01 -0600

    Update admin-login-page-script.php

--- HTML/admin-login-page-script.php
+++ HTML/admin-login-page-script.php
@@ -1,24 +1,39 @@
 <?php
-// Database connection logic here
+require_once 'init.php'; // Assuming this initializes your environment
+
+// Database connection details
+$host = "34.68.154.206";
+$database = "Post_Office_Schema";
+$user = "root";
+$password = "umapuma";
+
+// Create connection
+$conn = new mysqli($host, $user, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $adminEmail = $_POST['adminEmail'];
-    $adminPassword = $_POST['adminPassword'];
+    $adminEmail = $_POST['adminEmail']; // Assuming the form field for admin email is named 'adminEmail'
+    $adminPassword = $_POST['adminPassword']; // Assuming the form field for admin password is named 'adminPassword'
 
     // Validate and sanitize inputs
     // ...
 
     // Prepare and execute the query for admin login
-    $stmt = $conn->prepare("SELECT emp_password FROM EMPLOYEE WHERE email = ?");
+    $stmt = $conn->prepare("SELECT emp_password FROM EMPLOYEE WHERE email = ?"); // Replace 'EMPLOYEE' and 'emp_password' with your admin table and column names
     $stmt->bind_param("s", $adminEmail);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-        if ($adminPassword === $row['admin_password']) {
+        if ($adminPassword === $row['emp_password']) { // Replace 'emp_password' with the actual admin password column name
             // Admin authentication successful
-            // Redirect or start session, etc.
+            header("Location: admin-portal-nofications-page.html"); // Redirect to the admin portal page
+            exit;
         } else {
             echo "Invalid password.";
         }
@@ -30,4 +45,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conn->close();
 }
 ?>
-
