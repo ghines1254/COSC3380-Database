ea97c5fff1265a0045d63d88bdebace80e7ab598
ghines1254 <ghines1254@gmail.com>
2023-11-16 01:07:38 -0600

    dsaddad

--- HTML/connection.php
+++ HTML/connection.php
@@ -1,14 +1,22 @@
 <?php
+$servername = "34.68.154.206";
+$username = "root";
+$password = "umapuma";
+$database = "Post_Office_Schema";
 
-$dbhost = "34.68.154.206";
-$dbusername = "root";
-$dbpassword = "umapuma";
-$dbname = "Post_Office_Schema";
-$con = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
+try {
+    // Create connection
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
 
-if(!$con)
-{
-    die(" database connection failed");
-};
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Perform database operations here...
+
+    // Close connection (optional for PDO, as it will be automatically closed when script ends)
+    $conn = null;
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
 ?>
--- HTML/signupGrab.php
+++ HTML/signupGrab.php
@@ -1,31 +1,37 @@
 <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST")
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    function get_unique_id() 
     {
-        function get_unique_id() {
-            $uniqueRandomNumber = random_int(1000000000, 9999999999) . time();
-            return $uniqueRandomNumber;
-        }
-
-
-        $customerID = get_unique_id();
-        $firstName = $_POST['firstname'];
-        $lastName = $_POST['lastname'];
-        $middleInitial = $_POST['middleinitial'];
-        $address1 = $_POST['address-line-1'];
-        $address2 = $_POST['address-line-2'];
-        $city = $_POST['city1'];
-        $state = $_POST['state'];
-        $zipcode = $_POST['zip'];
-        $email = $_POST['email'];
-        $phoneNum = $_POST['phone1'];
-        $password = $_POST['password'];
-
-        echo $firstName . ' ' . $lastName;
-
-
+        $uniqueRandomNumber = random_int(1000000000, 9999999999) . time();
+        return $uniqueRandomNumber;
     }
-    else{
+
+
+    $customerID = get_unique_id();
+    $firstName = $_POST['firstname'];
+    $lastName = $_POST['lastname'];
+    $middleInitial = $_POST['middleinitial'];
+    $address1 = $_POST['address-line-1'];
+    $address2 = $_POST['address-line-2'];
+    $city = $_POST['city1'];
+    $state = $_POST['state'];
+    $zipcode = $_POST['zip'];
+    $email = $_POST['email'];
+    $phoneNum = $_POST['phone1'];
+    $password = $_POST['password'];
+
+    try
+    {
+        require_once "connection.php";
+
+        $query = "INSERT INTO CUSTOMER (customer_phone, customer_id, zip, state, street_address, city, first_name, last_name, email, PASSWORD) VALUES (phoneNum, customerID, zipcode, state, address1, city, firstName, lastName, email, password);";
+    }catch (Exception $e){
+        echo "Error: " . $e->getMessage();
+    }
+}
+else{
         header("Location: ../sign-up-page.php");
-    }
+}
 
 ?>
