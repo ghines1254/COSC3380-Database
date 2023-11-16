146fecc7f32ac6eabdce0c877d213e1df4473072
Z4K201909 <124842015+Z4K201909@users.noreply.github.com>
2023-11-16 03:04:14 -0600

    Update employee-delivery-script.php

--- HTML/employee-delivery-script.php
+++ HTML/employee-delivery-script.php
@@ -1,1 +1,35 @@
+<?php
+// Database connection details
+$host = "34.68.154.206";
+$database = "Post_Office_Schema";
+$user = "root";
+$password = "umapuma";
+
+// Create connection
+$conn = new mysqli($host, $user, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $packageId = $_POST['packageId'];
+    $status = $_POST['status'];
+
+    // Database update query
+    $stmt = $conn->prepare("UPDATE packages SET status = ? WHERE package_id = ?");
+    $stmt->bind_param("si", $status, $packageId);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        echo "Status updated successfully";
+    } else {
+        echo "Error updating status";
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
 
