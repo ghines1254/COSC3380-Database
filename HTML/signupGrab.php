86fad197e53ab15abd22624382e987e3dce46d17
ghines1254 <ghines1254@gmail.com>
2023-11-18 17:15:17 -0600

    dsada

--- HTML/signupGrab.php
+++ HTML/signupGrab.php
@@ -48,7 +48,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST")
 
     $stmt->execute();
     if ($stmt->affected_rows > 0) {
-        header("Location: account-page.php");
+        header("Location: account-page.html");
     } else {
         echo "Error inserting customer status or no changes made.";
     }
