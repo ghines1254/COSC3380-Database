49b4cab24a3b8c4989b0372be9091c20f0572cb8
Z4K201909 <124842015+Z4K201909@users.noreply.github.com>
2023-11-17 20:04:09 -0600

    Update employee-shipping-page-script.php

--- HTML/employee-shipping-page-script.php
+++ HTML/employee-shipping-page-script.php
@@ -1,11 +1,16 @@
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 
 
 
 <?php
 session_start(); // Start the session at the beginning of the script
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 
 // Database connection details
 $host = "34.68.154.206";
