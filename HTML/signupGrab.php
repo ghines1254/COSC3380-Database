6d62c2c303ec653dc9d17601d2a2c17f90336553
ghines1254 <ghines1254@gmail.com>
2023-11-18 17:21:39 -0600

    fixing signup

--- HTML/signupGrab.php
+++ HTML/signupGrab.php
@@ -48,7 +48,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST")
 
     $stmt->execute();
     if ($stmt->affected_rows > 0) {
-        header("Location: account-page.html");
+        header("Location:  customer-portal-nofications-page.html");
     } else {
         echo "Error inserting customer status or no changes made.";
     }
