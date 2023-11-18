09f6b1567a5c9f0a2e264556324d666848620fc6
Z4K201909 <124842015+Z4K201909@users.noreply.github.com>
2023-11-17 23:01:55 -0600

    Update employee-delivery-script.php

--- HTML/employee-delivery-script.php
+++ HTML/employee-delivery-script.php
@@ -1,38 +1,43 @@
 <?php
 require_once 'init.php';
 
-
-
 // Database connection details
 $host = "34.68.154.206";
 $database = "Post_Office_Schema";
 $user = "root";
 $password = "umapuma";
 
-// Create connection
-$conn = new mysqli($host, $user, $password, $database);
+try {
+    // Create connection
+    $conn = new mysqli($host, $user, $password, $database);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $trackingNumber = $_POST['trackingNumber'];
-    $newStatus = $_POST['newStatus'];
-
-    
-    $stmt = $conn->prepare("UPDATE PACKAGE SET status = ? WHERE tracking_number = ?");
-    $stmt->bind_param("ss", $newStatus, $trackingNumber);
-    $stmt->execute();
-
-    if ($stmt->affected_rows > 0) {
-        echo "Package status updated successfully.";
-    } else {
-        echo "Error updating package status or no changes made.";
+    // Check connection
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
     }
 
-    $stmt->close();
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $trackingNumber = $_POST['trackingNumber'];
+        $newStatus = $_POST['newStatus'];
+
+        $stmt = $conn->prepare("UPDATE PACKAGE SET status = ? WHERE tracking_number = ?");
+        if (!$stmt) {
+            throw new Exception("Prepare statement failed: " . $conn->error);
+        }
+        $stmt->bind_param("ss", $newStatus, $trackingNumber);
+        $stmt->execute();
+
+        if ($stmt->affected_rows > 0) {
+            echo "Package status updated successfully.";
+        } else {
+            echo "Error updating package status or no changes made.";
+        }
+
+        $stmt->close();
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+} finally {
     $conn->close();
 }
 ?>
