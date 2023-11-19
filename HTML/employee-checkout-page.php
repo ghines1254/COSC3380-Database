8639896821ec7d53466b1348c613203f001ea96d
valjester <94697099+valjester@users.noreply.github.com>
2023-11-19 01:44:22 -0600

    Update employee-checkout-page.php

--- HTML/employee-checkout-page.php
+++ HTML/employee-checkout-page.php
@@ -143,8 +143,9 @@ if (!$result)
 while ($row = $result->fetch_assoc()) 
 {
     echo "<div>";
-    echo "<span>{$row['product_id']}</span>";
-    echo "<span>{$row['product_name']}</span>";
+    echo "<span>{$row['product_id']} </span>";
+    echo "<span>{$row['product_name']} </span>";
+    echo "<span> Price: {$row['product_price']} </span>";
     echo "<span id='stock_{$row['product_id']}'>Stock: {$row['stock_remaining']}</span>";
     echo "<button onclick=\"updateStock('{$row['product_id']}', 'increment')\">+</button>";
     echo "<button onclick=\"updateStock('{$row['product_id']}', 'decrement')\">-</button>";
