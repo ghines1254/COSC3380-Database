1d97f8490128245f2c5825a56646decaadd6803a
valjester <94697099+valjester@users.noreply.github.com>
2023-11-18 22:19:58 -0600

    Update employee-checkout-page.php

--- HTML/employee-checkout-page.php
+++ HTML/employee-checkout-page.php
@@ -1,32 +1,11 @@
-<?php
-require once 'init.php';
-require once 'connection.php';
 
-$query = "SELECT product_id, product_name, product_price, stock_remaining FROM IN_STORE_PRODUCTS";
 
-$result = $conn->query($query);
-
-if (!$result) 
-{
-    die("Error retrieving product information: " . $conn->error);
-}
-
-while ($row = $result->fetch_assoc()) 
-{
-    echo "<div>";
-    echo "<span>{$row['product_name']}</span>";
-    echo "<span>Stock: {$row['stock_remaining']}</span>";
-    echo "<button onclick=\"updateStock({$row['product_id']}, 'increment')\">+</button>";
-    echo "<button onclick=\"updateStock({$row['product_id']}, 'decrement')\">-</button>";
-    echo "</div>";
-}
-
-$conn->close();
-?>
 
 <!DOCTYPE html>
 <html>
   <head>
+  
+
     <meta charset="utf-8" />
     <meta name="viewport" content="initial-scale=1, width=device-width" />
 
@@ -44,6 +23,13 @@ $conn->close();
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
     />
+
+    
+      <?php
+require once 'init.php';
+require once 'connection.php';
+        ?>
+      
   </head>
   <body>
     <div class="employee-checkout-page">
@@ -173,6 +159,33 @@ $conn->close();
       </div>
     </div>
 
+
+<?php
+$query = "SELECT product_id, product_name, product_price, stock_remaining FROM IN_STORE_PRODUCTS";
+
+$result = $conn->query($query);
+
+if (!$result) 
+{
+    die("Error retrieving product information: " . $conn->error);
+}
+
+while ($row = $result->fetch_assoc()) 
+{
+    echo "<div>";
+    echo "<span>{$row['product_name']}</span>";
+    echo "<span>Stock: {$row['stock_remaining']}</span>";
+    echo "<button onclick=\"updateStock({$row['product_id']}, 'increment')\">+</button>";
+    echo "<button onclick=\"updateStock({$row['product_id']}, 'decrement')\">-</button>";
+    echo "</div>";
+}
+
+$conn->close();
+?>
+
+
+
+      
     <script>
       var frameContainer1 = document.getElementById("frameContainer1");
       if (frameContainer1) {
