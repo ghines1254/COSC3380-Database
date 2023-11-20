33968445063ec42dee1fcd4e6d9e2c1cf9cedd74
valjester <94697099+valjester@users.noreply.github.com>
2023-11-20 16:07:03 -0600

    Update admin-employees-page.php

--- HTML/admin-employees-page.php
+++ HTML/admin-employees-page.php
@@ -381,7 +381,118 @@ while ($row = $result->fetch_assoc())
     echo "<td>{$row['branch_id']}</td>";
     echo "</tr>";
 }
-?> </tbody></table></div>
+?> </tbody></table>
+
+
+<form id="addEmployeeForm">
+    <label for="firstName">First Name:</label>
+    <input type="text" id="firstName" name="firstName" required>
+
+    <label for="middleInitial">Middle Initial:</label>
+    <input type="text" id="middleInitial" name="middleInitial">
+
+    <label for="lastName">Last Name:</label>
+    <input type="text" id="lastName" name="lastName" required>
+
+    <label for="employeeID">Employee ID:</label>
+    <input type="number" id="employeeID" name="employeeID" required>
+
+    <label for="gender">Sex:</label>
+    <select id="gender" name="gender" required>
+        <option value="M">Male</option>
+        <option value="F">Female</option>
+    </select>
+
+    <label for="birthdate">Birthdate:</label>
+    <input type="date" id="birthdate" name="birthdate" required>
+
+    <label for="streetAddress">Street Address:</label>
+    <input type="text" id="streetAddress" name="streetAddress" required>
+
+    <label for="city">City:</label>
+    <input type="text" id="city" name="city" required>
+
+    <label for="state">State:</label>
+    <select id="state" name="state" required>
+                      <option value="AL">AL</option>
+                      <option value="AK">AK</option>
+                      <option value="AR">AR</option>
+                      <option value="AZ">AZ</option>
+                      <option value="CA">CA</option>
+                      <option value="CO">CO</option>
+                      <option value="CT">CT</option>
+                      <option value="DC">DC</option>
+                      <option value="DE">DE</option>
+                      <option value="FL">FL</option>
+                      <option value="GA">GA</option>
+                      <option value="HI">HI</option>
+                      <option value="IA">IA</option>
+                      <option value="ID">ID</option>
+                      <option value="IL">IL</option>
+                      <option value="IN">IN</option>
+                      <option value="KS">KS</option>
+                      <option value="KY">KY</option>
+                      <option value="LA">LA</option>
+                      <option value="MA">MA</option>
+                      <option value="MD">MD</option>
+                      <option value="ME">ME</option>
+                      <option value="MI">MI</option>
+                      <option value="MN">MN</option>
+                      <option value="MO">MO</option>
+                      <option value="MS">MS</option>
+                      <option value="MT">MT</option>
+                      <option value="NC">NC</option>
+                      <option value="NE">NE</option>
+                      <option value="NH">NH</option>
+                      <option value="NJ">NJ</option>
+                      <option value="NM">NM</option>
+                      <option value="NV">NV</option>
+                      <option value="NY">NY</option>
+                      <option value="ND">ND</option>
+                      <option value="OH">OH</option>
+                      <option value="OK">OK</option>
+                      <option value="OR">OR</option>
+                      <option value="PA">PA</option>
+                      <option value="RI">RI</option>
+                      <option value="SC">SC</option>
+                      <option value="SD">SD</option>
+                      <option value="TN">TN</option>
+                      <option value="TX">TX</option>
+                      <option value="UT">UT</option>
+                      <option value="VT">VT</option>
+                      <option value="VA">VA</option>
+                      <option value="WA">WA</option>
+                      <option value="WI">WI</option>
+                      <option value="WV">WV</option>
+                      <option value="WY">WY</option>
+        
+    </select>
+
+     <label for="zipcode">Zipcode:</label>
+    <input type="text" id="zipcode" name="zipcode" pattern="\d+" required>
+
+    <label for="phone">Phone:</label>
+    <input type="text" id="phone" name="phone" required>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
+
+    <label for="ssn">SSN:</label>
+    <input type="text" id="ssn" name="ssn" pattern="\d{9}" required>
+
+    <label for="department">Department (optional):</label>
+    <input type="text" id="department" name="department">
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+
+    <button type="button" onclick="addEmployee()">Add Employee</button>
+</form>
+
+
+
+
+</div>
 
         <script>
             var frameContainer1 = document.getElementById("frameContainer1");
@@ -430,15 +541,10 @@ while ($row = $result->fetch_assoc())
 
             if (button4) {
                 button4.addEventListener("click", function (e) {
-                    fetch('get-employee-list.php')
-                    .then(response => {
-                if (!response.ok) {
-                    alert("Not ok");
-                    throw new Error('Network response was not ok');
-                }
+                    fetch('get-employee-list.php')         }
 
                 return response.json();
-            })
+            }
             .then(data => {
 
                 console.log(data);
