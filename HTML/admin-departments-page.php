3f62d9e3508da14f6f313dd0b88aa9f9621cd21b
Z4K201909 <124842015+Z4K201909@users.noreply.github.com>
2023-11-20 17:15:01 -0600

    Update admin-departments-page.php

--- HTML/admin-departments-page.php
+++ HTML/admin-departments-page.php
@@ -1,139 +1,141 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="initial-scale=1, width=device-width" />
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="initial-scale=1, width=device-width" />
+  <link rel="stylesheet" href="./global.css" />
+  <link rel="stylesheet" href="./admin-departments-page.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@400;500;600;700;800;900&display=swap" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700&display=swap" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" />
+  <style>
+    .report-table {
+      width: 100%;
+      max-width: 700px;
+      border-collapse: collapse;
+      margin-left: auto;
+      margin-right: auto;
+    }
+    .report-table th, .report-table td {
+      border: 1px solid black;
+      padding: 8px;
+      text-align: left;
+    }
+  </style>
+</head>
+<body>
+  <div class="admin-departments-page">
+    <div class="minibackground1">
+      <img class="image-1-icon1" alt="" src="./public/image-12@2x.png" />
+    </div>
 
-    <link rel="stylesheet" href="./global.css" />
-    <link rel="stylesheet" href="./admin-departments-page.css" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Lexend Deca:wght@400;500;600;700;800;900&display=swap"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700&display=swap"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
-    />
-  </head>
-  <body>
-    <div class="admin-departments-page">
-      <div class="minibackground1">
-        <img class="image-1-icon1" alt="" src="./public/image-12@2x.png" />
-      </div>
-
-      <div class="navigation-bar-light1">
-        <div class="navigation-bar1"></div>
-        <div class="navigation-bar-light-child">
-          <div class="rectangle-group">
-            <div class="group-item"></div>
-            <b class="login1">Login</b>
-          </div>
-        </div>
-        <div class="cougarcourier1-4-group" id="frameContainer1">
-          <img
-            class="cougarcourier1-4-icon1"
-            alt=""
-            src="./public/cougarcourier1-4@2x.png"
-          />
-          <b class="cougar-courier1">Cougar Courier</b>
+    <div class="navigation-bar-light1">
+      <div class="navigation-bar1"></div>
+      <div class="navigation-bar-light-child">
+        <div class="rectangle-group">
+          <div class="group-item"></div>
+          <b class="login1">Login</b>
         </div>
       </div>
+      <div class="cougarcourier1-4-group" id="frameContainer1">
+        <img class="cougarcourier1-4-icon1" alt="" src="./public/cougarcourier1-4@2x.png" />
+        <b class="cougar-courier1">Cougar Courier</b>
+      </div>
+    </div>
 
-      <div class="admin-portal-outline">
-        <div class="frame-parent">
-          <div class="rectangle-container">
-            <div class="frame-child"></div>
-            <div class="welcome-to-your-portal-page-wrapper">
-              <b class="welcome-to-your">Departments</b>
-            </div>
+    <div class="admin-portal-outline">
+      <div class="frame-parent">
+        <div class="rectangle-container">
+          <div class="frame-child"></div>
+          <div class="welcome-to-your-portal-page-wrapper">
+            <b class="welcome-to-your">Departments</b>
           </div>
-          <div class="frame-group">
-            <div class="frame-wrapper">
-              <div class="button2-parent">
-                <div class="button2" id="button2Container">
-                  <div class="button2-child"></div>
-                  <b class="departments">PO Locations</b>
-                </div>
-                <div class="button3" id="button3Container">
-                  <div class="button3-child"></div>
-                  <b class="departments">Products</b>
-                </div>
-                <div class="button4">
-                  <div class="button2-child"></div>
-                  <b class="departments">Departments</b>
-                </div>
-                <div class="button5" id="button5Container">
-                  <div class="button2-child"></div>
-                  <b class="departments">Employees</b>
-                </div>
-                <div class="button7" id="button7Container">
-                  <div class="button2-child"></div>
-                  <b class="departments">Account</b>
-                </div>
+        </div>
+        <div class="frame-group">
+          <div class="frame-wrapper">
+            <div class="button2-parent">
+              <div class="button2" id="button2Container">
+                <div class="button2-child"></div>
+                <b class="departments">PO Locations</b>
+              </div>
+              <div class="button3" id="button3Container">
+                <div class="button3-child"></div>
+                <b class="departments">Products</b>
+              </div>
+              <div class="button4">
+                <div class="button2-child"></div>
+                <b class="departments">Departments</b>
+              </div>
+              <div class="button5" id="button5Container">
+                <div class="button2-child"></div>
+                <b class="departments">Employees</b>
+              </div>
+              <div class="button7" id="button7Container">
+                <div class="button2-child"></div>
+                <b class="departments">Account</b>
               </div>
             </div>
-            <div class="portal-page"></div>
           </div>
+          <div class="portal-page"></div>
+        </div>
 
-          <!-- Centered Generate Report Form -->
-          <div class="generate-report-form" style="text-align: center; padding: 20px;">
-            <form method="post" action="">
-              <input type="submit" name="generate_report" value="Generate Report">
-            </form>
-          </div>
-
-          <!-- Employee Data Display inside frame-parent -->
-          <div class="employee-data-report" style="margin-top: 20px;">
-            <?php
-            include 'admin-departments-page-script.php';
-            if (isset($_POST['generate_report'])) {
-              $employeeData = fetchEmployeeReport();
-              if (!empty($employeeData)): ?>
-                <table>
-                  <tr>
-                    <th>ID</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Sex</th>
-                    <th>Birthdate</th>
-                    <th>City</th>
-                    <th>State</th>
-                    <th>Zipcode</th>
-                    <th>Department</th>
-                    <th>Created On</th>
-                    <th>Packages Delivered</th>
-                  </tr>
-                  <?php foreach ($employeeData as $data): ?>
-                    <tr>
-                      <td><?= htmlspecialchars($data['idnum']) ?></td>
-                      <td><?= htmlspecialchars($data['first_name']) ?></td>
-                      <td><?= htmlspecialchars($data['last_name']) ?></td>
-                      <td><?= htmlspecialchars($data['sex']) ?></td>
-                      <td><?= htmlspecialchars($data['birthdate']) ?></td>
-                      <td><?= htmlspecialchars($data['city']) ?></td>
-                      <td><?= htmlspecialchars($data['state']) ?></td>
-                      <td><?= htmlspecialchars($data['zipcode']) ?></td>
-                      <td><?= htmlspecialchars($data['dept']) ?></td>
-                      <td><?= htmlspecialchars($data['created_on']) ?></td>
-                      <td><?= htmlspecialchars($data['packages_delivered']) ?></td>
-                    </tr>
-                  <?php endforeach; ?>
-                </table>
-              <?php else: ?>
-                <p>No employee data found.</p>
-              <?php endif;
-            }
-            ?>
-          </div>
+        <!-- Centered Generate Report Form -->
+        <div class="generate-report-form" style="text-align: center; padding: 20px;">
+          <form method="post" action="">
+            <input type="submit" name="generate_report" value="Generate Report">
+          </form>
         </div>
       </div>
     </div>
 
+    <!-- Employee Data Display outside of admin-portal-outline -->
+    <div class="employee-data-report" style="margin-top: 20px;">
+      <?php
+      include 'admin-departments-page-script.php';
+      if (isset($_POST['generate_report'])) {
+        $employeeData = fetchEmployeeReport();
+        if (!empty($employeeData)): ?>
+          <table class="report-table">
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Sex</th>
+              <th>Birthdate</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Zipcode</th>
+              <th>Department</th>
+              <th>Created On</th>
+              <th>Packages Delivered</th>
+            </tr>
+            <?php foreach ($employeeData as $data): ?>
+            <tr>
+              <td><?= htmlspecialchars($data['idnum']) ?></td>
+              <td><?= htmlspecialchars($data['first_name']) ?></td>
+              <td><?= htmlspecialchars($data['last_name']) ?></td>
+              <td><?= htmlspecialchars($data['sex']) ?></td>
+              <td><?= htmlspecialchars($data['birthdate']) ?></td>
+              <td><?= htmlspecialchars($data['city']) ?></td>
+              <td><?= htmlspecialchars($data['state']) ?></td>
+              <td><?= htmlspecialchars($data['zipcode']) ?></td>
+              <td><?= htmlspecialchars($data['dept']) ?></td>
+              <td><?= htmlspecialchars($data['created_on']) ?></td>
+              <td><?= htmlspecialchars($data['packages_delivered']) ?></td>
+            </tr>
+            <?php endforeach; ?>
+          </table>
+        <?php else: ?>
+          <p>No employee data found.</p>
+        <?php endif;
+      }
+      ?>
+    </div>
+
     <img class="vector-icon" alt="" src="./public/vector15.svg" id="vector" />
+
+  
+
 
     <script>
       var frameContainer1 = document.getElementById("frameContainer1");
