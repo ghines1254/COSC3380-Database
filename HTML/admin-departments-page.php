677639f14c43f6932e4f0d1f79923fdebc7e431a
Z4K201909 <124842015+Z4K201909@users.noreply.github.com>
2023-11-20 16:45:28 -0600

    Update admin-departments-page.php

--- HTML/admin-departments-page.php
+++ HTML/admin-departments-page.php
@@ -29,89 +29,6 @@
         </div>
 
         <!-- Employee Data Display -->
-<!--         <div class="employee-data-report" style="margin-top: 20px;">
-            <?php
-            include 'admin-departments-page-script.php';
-
-            if (isset($_POST['generate_report'])) {
-                $employeeData = fetchEmployeeReport();
-                
-                if (!empty($employeeData)): ?>
-                    <table>
-                        <tr>
-                            <th>ID</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Sex</th>
-                            <th>Birthdate</th>
-                            <th>City</th>
-                            <th>State</th>
-                            <th>Zipcode</th>
-                            <th>Department</th>
-                            <th>Created On</th>
-                            <th>Packages Delivered</th>
-                        </tr>
-                        <?php foreach ($employeeData as $data): ?>
-                            <tr>
-                                <td><?= htmlspecialchars($data['idnum']) ?></td>
-                                <td><?= htmlspecialchars($data['first_name']) ?></td>
-                                <td><?= htmlspecialchars($data['last_name']) ?></td>
-                                <td><?= htmlspecialchars($data['sex']) ?></td>
-                                <td><?= htmlspecialchars($data['birthdate']) ?></td>
-                                <td><?= htmlspecialchars($data['city']) ?></td>
-                                <td><?= htmlspecialchars($data['state']) ?></td>
-                                <td><?= htmlspecialchars($data['zipcode']) ?></td>
-                                <td><?= htmlspecialchars($data['dept']) ?></td>
-                                <td><?= htmlspecialchars($data['created_on']) ?></td>
-                                <td><?= htmlspecialchars($data['packages_delivered']) ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </table>
-                <?php else: ?>
-                    <p>No employee data found.</p>
-                <?php endif;
-            }
-            ?>
-        </div>
-    </div>
- -->
-
-    
-    <div class="admin-departments-page">
-      <div class="admin-departments-page-child">
-
-
-
-        
-      </div>
-      <div class="minibackground1">
-        <img class="image-1-icon1" alt="" src="./public/image-12@2x.png" />
-
-
-
-        
-      </div>
-      <div class="navigation-bar-light1">
-        <div class="navigation-bar1"></div>
-        <div class="navigation-bar-light-child">
-          <div class="rectangle-group">
-            <div class="group-item"></div>
-            <b class="login1">Login</b>
-          </div>
-        </div>
-        <div class="cougarcourier1-4-group" id="frameContainer1">
-          <img
-            class="cougarcourier1-4-icon1"
-            alt=""
-            src="./public/cougarcourier1-4@2x.png"
-          />
-
-          <b class="cougar-courier1">Cougar Courier</b>
-        </div>
-      </div>
-      <div class="admin-portal-outline">
-        <div class="frame-parent">
-   <!-- Employee Data Display -->
         <div class="employee-data-report" style="margin-top: 20px;">
             <?php
             include 'admin-departments-page-script.php';
@@ -159,8 +76,44 @@
     </div>
 
 
+    
+    <div class="admin-departments-page">
+      <div class="admin-departments-page-child">
 
-          
+
+
+        
+      </div>
+      <div class="minibackground1">
+        <img class="image-1-icon1" alt="" src="./public/image-12@2x.png" />
+
+
+
+        
+      </div>
+      <div class="navigation-bar-light1">
+        <div class="navigation-bar1"></div>
+        <div class="navigation-bar-light-child">
+          <div class="rectangle-group">
+            <div class="group-item"></div>
+            <b class="login1">Login</b>
+          </div>
+        </div>
+        <div class="cougarcourier1-4-group" id="frameContainer1">
+          <img
+            class="cougarcourier1-4-icon1"
+            alt=""
+            src="./public/cougarcourier1-4@2x.png"
+          />
+
+          <b class="cougar-courier1">Cougar Courier</b>
+        </div>
+      </div>
+      <div class="admin-portal-outline">
+        <div class="frame-parent">
+
+
+       
           <div class="rectangle-container">
             <div class="frame-child"></div>
             <div class="welcome-to-your-portal-page-wrapper">
