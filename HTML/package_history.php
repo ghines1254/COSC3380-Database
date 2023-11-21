a25929c18fe7d5d919eb8daef01510aab7cc89ad
zzclark17 <zzclark17@gmail.com>
2023-11-21 14:01:38 -0600

    testing

--- HTML/package_history.php
+++ HTML/package_history.php
@@ -1,53 +1,40 @@
 <?php
 require_once 'init.php';
 
-// Retrieve the tracking number and filter parameters from the GET parameters
 $trackingNumber = isset($_GET['tracking_number']) ? $_GET['tracking_number'] : '';
 $startDate = isset($_GET['startDate']) ? $_GET['startDate'] : '';
 $endDate = isset($_GET['endDate']) ? $_GET['endDate'] : '';
 $employeeId = isset($_GET['employeeId']) ? $_GET['employeeId'] : '';
 
-// base query
-$query = "SELECT * FROM PACKAGE_HISTORY WHERE package_id = ?";
+// Define the JOIN query to fetch from both tables
+$query = "
+    SELECT 
+        ph.package_id, ph.emp_id, ph.location_type, ph.location, ph.vin, ph.time_scanned,
+        ti.on_truck, ti.starting_location, ti.received, ti.delivered_by, ti.created_on, ti.last_updated, ti.eta
+    FROM 
+        PACKAGE_HISTORY ph
+    LEFT JOIN 
+        TRACKING_INFO ti ON ph.package_id = ti.package_id
+    WHERE 
+        ph.package_id = ?";
 
-// Add conditions based on filters
-$params = [$trackingNumber]; // Start with tracking number
-$paramTypes = 's'; // Tracking number is a string
+// Additional filter conditions can be added to the WHERE clause of your SQL query here
 
-if (!empty($startDate) && !empty($endDate)) {
-    $query .= " AND time_scanned BETWEEN ? AND ?";
-    $params[] = $startDate;
-    $params[] = $endDate;
-    $paramTypes .= 'ss'; // Two string parameters for dates
-}
-
-if (!empty($employeeId)) {
-    $query .= " AND emp_id = ?";
-    $params[] = $employeeId;
-    $paramTypes .= 's'; // Another string for employee ID
-}
-
-// Prepare the SQL statement
 $stmt = $conn->prepare($query);
-
-// Dynamic binding of parameters
-$stmt->bind_param($paramTypes, ...$params);
-
-// Execute the query
+$stmt->bind_param('s', $trackingNumber);
 $stmt->execute();
-
-// Fetch the results
 $result = $stmt->get_result();
 
-// Check if records are found
+$records = [];
+
 if ($result->num_rows > 0) {
-    // Fetch all records as an associative array
-    $historyRecords = $result->fetch_all(MYSQLI_ASSOC);
-} else {
-    $historyRecords = array();
+    while ($row = $result->fetch_assoc()) {
+        $records[] = $row;
+    }
 }
 
-// Don't close the statement and connection yet if we may use them again after form submission
+$stmt->close();
+$conn->close();
 ?>
 
 <!DOCTYPE html>
@@ -55,56 +42,74 @@ if ($result->num_rows > 0) {
 <head>
     <meta charset="UTF-8">
     <title>Package History</title>
-    <!-- add stylesheet here -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Make sure to replace 'style.css' with the actual CSS file name -->
 </head>
 <body>
     <h1>Package History for Tracking Number: <?php echo htmlspecialchars($trackingNumber); ?></h1>
+    <!-- Your existing form for filters -->
+    <!-- ... -->
 
-    <!-- Filter Form -->
-    <form action="package_history.php" method="get">
-        <input type="hidden" name="tracking_number" value="<?php echo htmlspecialchars($trackingNumber); ?>">
-        
-        <label for="startDate">Start Date:</label>
-        <input type="date" id="startDate" name="startDate" value="<?php echo htmlspecialchars($startDate); ?>">
-        
-        <label for="endDate">End Date:</label>
-        <input type="date" id="endDate" name="endDate" value="<?php echo htmlspecialchars($endDate); ?>">
-        
-        <label for="employeeId">Employee ID:</label>
-        <input type="text" id="employeeId" name="employeeId" value="<?php echo htmlspecialchars($employeeId); ?>">
-        
-        <input type="submit" value="Filter">
-    </form>
-
-    <!-- Table of Package History Records -->
-    <?php if (count($historyRecords) > 0): ?>
+    <?php if (count($records) > 0): ?>
+        <h2>Package History</h2>
         <table>
+            <!-- Headers for PACKAGE_HISTORY -->
             <thead>
                 <tr>
+                    <th>Package ID</th>
                     <th>Employee ID</th>
                     <th>Location Type</th>
                     <th>Location</th>
+                    <th>VIN</th>
                     <th>Time Scanned</th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($historyRecords as $record): ?>
-                    <tr>
-                        <td><?php echo htmlspecialchars($record['emp_id']); ?></td>
-                        <td><?php echo htmlspecialchars($record['location_type']); ?></td>
-                        <td><?php echo htmlspecialchars($record['location']); ?></td>
-                        <td><?php echo htmlspecialchars($record['time_scanned']); ?></td>
-                    </tr>
+                <!-- Data for PACKAGE_HISTORY -->
+                <?php foreach ($records as $record): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($record['package_id']); ?></td>
+                    <td><?php echo htmlspecialchars($record['emp_id']); ?></td>
+                    <td><?php echo htmlspecialchars($record['location_type']); ?></td>
+                    <td><?php echo htmlspecialchars($record['location']); ?></td>
+                    <td><?php echo htmlspecialchars($record['vin']); ?></td>
+                    <td><?php echo htmlspecialchars($record['time_scanned']); ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <h2>Tracking Info</h2>
+        <table>
+            <!-- Headers for TRACKING_INFO -->
+            <thead>
+                <tr>
+                    <th>On Truck</th>
+                    <th>Starting Location</th>
+                    <th>Received</th>
+                    <th>Delivered By</th>
+                    <th>Created On</th>
+                    <th>Last Updated</th>
+                    <th>ETA</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Data for TRACKING_INFO -->
+                <?php foreach ($records as $record): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($record['on_truck']); ?></td>
+                    <td><?php echo htmlspecialchars($record['starting_location']); ?></td>
+                    <td><?php echo htmlspecialchars($record['received']); ?></td>
+                    <td><?php echo htmlspecialchars($record['delivered_by']); ?></td>
+                    <td><?php echo htmlspecialchars($record['created_on']); ?></td>
+                    <td><?php echo htmlspecialchars($record['last_updated']); ?></td>
+                    <td><?php echo htmlspecialchars($record['eta']); ?></td>
+                </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
     <?php else: ?>
-        <p>No history records found for this tracking number.</p>
+        <p>No records found for this tracking number.</p>
     <?php endif; ?>
-
-    <?php
-    $stmt->close();
-    $conn->close();
-    ?>
 </body>
 </html>
