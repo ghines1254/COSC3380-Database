a5df7525b8959e75072561868c43e2abb06c10f3
zzclark17 <zzclark17@gmail.com>
2023-11-21 15:39:17 -0600

    Lets see if this breaks anything

--- HTML/package_history.php
+++ HTML/package_history.php
@@ -4,83 +4,69 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 $trackingNumber = isset($_GET['tracking_number']) ? $_GET['tracking_number'] : '';
-$selectedAttribute = isset($_GET['attribute']) ? $_GET['attribute'] : '';
-$startDate = isset($_GET['startDate']) ? $_GET['startDate'] : '';
-$endDate = isset($_GET['endDate']) ? $_GET['endDate'] : '';
+$selectedAttributes = isset($_POST['attributes']) ? $_POST['attributes'] : [];
+$startDate = isset($_POST['startDate']) ? $_POST['startDate'] : '';
+$endDate = isset($_POST['endDate']) ? $_POST['endDate'] : '';
 
-// Define an array of attributes available in both "Tracking Info" and "Package History"
-$attributes = [
+// Define an array of all columns in PACKAGE_HISTORY
+$allAttributes = [
+    'Package ID',
     'Employee ID',
+    'Location Type',
     'Location',
-    'Starting Location',
-    // Add more attributes as needed
+    'VIN',
+    'Time Scanned',
 ];
 
 // Initialize an empty array for params
 $params = [];
 
-// Create a function to build the WHERE clause based on the selected attribute
-function buildWhereClause($selectedAttribute, &$params) {
-    switch ($selectedAttribute) {
-        case 'Employee ID':
-            $whereClause = " WHERE ph.emp_id = ?";
-            break;
-        case 'Location':
-            $whereClause = " WHERE ph.location = ?";
-            break;
-        case 'Starting Location':
-            $whereClause = " WHERE ti.starting_location_id = ?";
-            break;
-        // Add more cases for other attributes as needed
-        default:
-            $whereClause = "";
+// Create a function to build the SELECT clause based on selected attributes
+function buildSelectClause($selectedAttributes) {
+    $selectClause = 'package_id, time_scanned'; // Always include Package ID and Time Scanned
+    foreach ($selectedAttributes as $attribute) {
+        $selectClause .= ', ' . $attribute;
     }
-    // Add selected attribute to params
-    $params[] = $selectedAttribute;
+    return $selectClause;
+}
+
+// Create a function to build the WHERE clause based on start and end dates
+function buildWhereClause($startDate, $endDate, &$params) {
+    $whereClause = '';
+    if (!empty($startDate) && !empty($endDate)) {
+        $whereClause = "WHERE time_scanned BETWEEN ? AND ?";
+        $params[] = $startDate;
+        $params[] = $endDate;
+    }
     return $whereClause;
 }
 
-$query = "
-    SELECT 
-        ph.package_id, ph.emp_id, ph.location_type, ph.location, ph.vin, ph.time_scanned,
-        ti.on_truck, ti.starting_location_id, ti.received, ti.delivered_by, ti.created_on, ti.last_updated, ti.eta
-    FROM 
-        PACKAGE_HISTORY ph
-    LEFT JOIN 
-        TRACKING_INFO ti ON ph.package_id = ti.package_id";
-
-// Apply attribute filter if a specific attribute is selected
-if (!empty($selectedAttribute)) {
-    $whereClause = buildWhereClause($selectedAttribute, $params);
-    $query .= $whereClause;
-}
-
-// Apply date filters for the "Package History" section
-if (!empty($startDate) && !empty($endDate)) {
-    if (strpos($query, 'WHERE') !== false) {
-        $query .= " AND ph.time_scanned BETWEEN ? AND ?";
-    } else {
-        $query .= " WHERE ph.time_scanned BETWEEN ? AND ?";
+// Check if "Get Package History!" button is clicked
+if (isset($_POST['getHistory'])) {
+    // Build SELECT clause based on selected attributes
+    $selectClause = buildSelectClause($selectedAttributes);
+    
+    // Build WHERE clause based on start and end dates
+    $whereClause = buildWhereClause($startDate, $endDate, $params);
+    
+    // Prepare and execute the SQL query
+    $query = "SELECT $selectClause FROM PACKAGE_HISTORY $whereClause";
+    $stmt = $conn->prepare($query);
+    
+    // Bind parameters dynamically
+    if ($params) {
+        $paramTypes = str_repeat('s', count($params));
+        $stmt->bind_param($paramTypes, ...$params);
     }
-    // Add start and end dates to params
-    $params[] = $startDate;
-    $params[] = $endDate;
+    
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    $records = $result->num_rows > 0 ? $result->fetch_all(MYSQLI_ASSOC) : [];
+    
+    $stmt->close();
 }
 
-$stmt = $conn->prepare($query);
-
-// Bind parameters dynamically
-if ($params) {
-    $paramTypes = str_repeat('s', count($params));
-    $stmt->bind_param($paramTypes, ...$params);
-}
-
-$stmt->execute();
-$result = $stmt->get_result();
-
-$records = $result->num_rows > 0 ? $result->fetch_all(MYSQLI_ASSOC) : [];
-
-$stmt->close();
 $conn->close();
 ?>
 
@@ -95,99 +81,55 @@ $conn->close();
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@400;500;600;700;800;900&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap">
-    <style>
-        /* Add CSS styles for hiding columns */
-        .hidden-column {
-            display: none;
-        }
-    </style>
 </head>
 <body>
     <h1>Package History for Tracking Number: <?php echo htmlspecialchars($trackingNumber); ?></h1>
 
-    <!-- Filter Form -->
-    <form action="package_history.php" method="get">
-        <input type="hidden" name="tracking_number" value="<?php echo htmlspecialchars($trackingNumber); ?>">
-        <label for="attribute">Select Attribute:</label>
-        <select id="attribute" name="attribute">
-            <option value="">-- Select Attribute --</option>
-            <?php foreach ($attributes as $attribute): ?>
-                <option value="<?php echo htmlspecialchars($attribute); ?>" <?php echo ($selectedAttribute == $attribute) ? 'selected' : ''; ?>>
-                    <?php echo htmlspecialchars($attribute); ?>
-                </option>
-            <?php endforeach; ?>
-        </select>
+    <!-- Display Tracking Info -->
+    <h2>Tracking Info</h2>
+    <!-- Replace with your code to display "Tracking Info" here -->
+
+    <!-- Display Package History Selection -->
+    <h2>Package History</h2>
+    <form action="package_history.php" method="post">
+        <?php foreach ($allAttributes as $attribute): ?>
+            <label>
+                <input type="checkbox" name="attributes[]" value="<?php echo htmlspecialchars($attribute); ?>" <?php echo in_array($attribute, $selectedAttributes) ? 'checked' : ''; ?>>
+                <?php echo htmlspecialchars($attribute); ?>
+            </label>
+        <?php endforeach; ?>
         <label for="startDate">Start Date:</label>
         <input type="date" id="startDate" name="startDate" value="<?php echo htmlspecialchars($startDate); ?>">
         <label for="endDate">End Date:</label>
         <input type="date" id="endDate" name="endDate" value="<?php echo htmlspecialchars($endDate); ?>">
-        <input type="submit" value="Filter">
+        <input type="submit" name="getHistory" value="Get Package History!">
     </form>
 
-    <!-- Display Tracking Info -->
-    <table>
-        <caption>Tracking Info</caption>
-        <thead>
-            <tr>
-                <th>On Truck</th>
-                <th>Starting Location</th>
-                <th>Received</th>
-                <th>Delivered By Employee ID</th>
-                <th>Created On</th>
-                <th>Last Updated</th>
-                <th>ETA</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($records as $record): ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($record['on_truck']); ?></td>
-                    <td><?php echo htmlspecialchars($record['starting_location_id']); ?></td>
-                    <td><?php echo ($record['received'] == 1) ? 'YES' : 'NO'; ?></td>
-                    <td><?php echo htmlspecialchars($record['delivered_by']); ?></td>
-                    <td><?php echo htmlspecialchars($record['created_on']); ?></td>
-                    <td><?php echo htmlspecialchars($record['last_updated']); ?></td>
-                    <td><?php echo htmlspecialchars($record['eta']); ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-
     <!-- Display Package History -->
-    <table>
-        <caption>Package History</caption>
-        <thead>
-            <tr>
-                <th>Package ID</th>
-                <?php if (!empty($selectedAttribute)): ?>
-                    <th><?php echo htmlspecialchars($selectedAttribute); ?></th>
-                <?php endif; ?>
-                <th class="hidden-column">Employee ID</th>
-                <th class="hidden-column">Location</th>
-                <th class="hidden-column">Starting Location</th>
-                <th>Location Type</th>
-                <th>Location</th>
-                <th>Truck Number</th>
-                <th>Time Scanned</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($records as $record): ?>
+    <?php if (isset($records)): ?>
+        <h2>Package History Results</h2>
+        <table>
+            <thead>
                 <tr>
-                    <td><?php echo htmlspecialchars($record['package_id']); ?></td>
-                    <?php if (!empty($selectedAttribute)): ?>
-                        <td><?php echo htmlspecialchars($record[$selectedAttribute]); ?></td>
-                    <?php endif; ?>
-                    <td class="hidden-column"><?php echo htmlspecialchars($record['emp_id']); ?></td>
-                    <td class="hidden-column"><?php echo htmlspecialchars($record['location']); ?></td>
-                    <td class="hidden-column"><?php echo htmlspecialchars($record['starting_location_id']); ?></td>
-                    <td><?php echo htmlspecialchars($record['location_type']); ?></td>
-                    <td><?php echo htmlspecialchars($record['location']); ?></td>
-                    <td><?php echo htmlspecialchars($record['vin']); ?></td>
-                    <td><?php echo htmlspecialchars($record['time_scanned']); ?></td>
+                    <?php foreach ($selectedAttributes as $attribute): ?>
+                        <th><?php echo htmlspecialchars($attribute); ?></th>
+                    <?php endforeach; ?>
+                    <th>Package ID</th>
+                    <th>Time Scanned</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($records as $record): ?>
+                    <tr>
+                        <?php foreach ($selectedAttributes as $attribute): ?>
+                            <td><?php echo htmlspecialchars($record[$attribute]); ?></td>
+                        <?php endforeach; ?>
+                        <td><?php echo htmlspecialchars($record['package_id']); ?></td>
+                        <td><?php echo htmlspecialchars($record['time_scanned']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
 </body>
 </html>
