d78a5ddd237a29d84637e37dfe00ff8109909123
valjester <94697099+valjester@users.noreply.github.com>
2023-11-25 02:17:15 -0600

    Update updateStock.php

--- HTML/updateStock.php
+++ HTML/updateStock.php
@@ -8,27 +8,57 @@ require_once "connection.php";
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $productId = $_POST['productId'];
     $action = $_POST['action'];
+    $priceQuery = "SELECT product_price FROM IN_STORE_PRODUCTS WHERE product_id = ?";
+    $priceStmt = $conn->prepare($priceQuery);
+    $priceStmt->bind_param('s', $productId);
+    $priceStmt->execute();
+
+    $priceResult = $priceStmt->get_result();
+    $priceRow = $priceResult->fetch_assoc();
+    $price = $priceRow['product_price'];
+
+    
 
     if ($action === 'increment') {
-        $query = "UPDATE IN_STORE_PRODUCTS SET stock_remaining = stock_remaining + 1 WHERE product_id = ?";
-    } elseif ($action === 'decrement') {
-        $query = "UPDATE IN_STORE_PRODUCTS SET stock_remaining = stock_remaining - 1 WHERE product_id = ?";
-    }
-
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param('s', $productId);  
-    $stmt->execute();
+        $updateQuery = "UPDATE IN_STORE_PRODUCTS SET stock_remaining = stock_remaining + 1 WHERE product_id = ?";
+        $stmt = $conn->prepare($updateQuery);
+        $stmt->bind_param('s', $productId);  
+        $stmt->execute();
    
-    if ($stmt->affected_rows > 0) {
+        if ($stmt->affected_rows > 0) {
         $updatedStock = getUpdatedStock($conn, $productId);
         echo $updatedStock;
-    } else {
+        } else {
         $error_message = $conn->error;
         echo "Error updating stock: " . $error_message;
         echo "@error_message:$error_message";
     }
-
     $conn->close();
+        
+    } elseif ($action === 'decrement') {
+        $updateQuery = "UPDATE IN_STORE_PRODUCTS SET stock_remaining = stock_remaining - 1 WHERE product_id = ?";
+        $salesQuery = "INSERT INTO SALES(product_id, product_price, branch_id, date_of_sale) VALUES (?, ?, ?, now())";
+        $branchId = $_SESSION['branch_id'];
+
+        $stmt = $conn->prepare($updateQuery);
+        $stmt->bind_param('s', $productId);  
+        $stmt->execute();
+   
+        if ($stmt->affected_rows > 0) {
+            $salesStmt = $conn->prepare($salesQuery);
+            $salesStmt->bind_param('sss', $productId, $price, $branchId);
+            $salesStmt->execute();
+            $updatedStock = getUpdatedStock($conn, $productId);
+            echo $updatedStock;
+        } else {
+        $error_message = $conn->error;
+        echo "Error updating stock: " . $error_message;
+        echo "@error_message:$error_message";
+    }
+    $conn->close();
+        
+    }
+
 } else {
     echo "Invalid request method";
 }
