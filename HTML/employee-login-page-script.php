9622043da68e93b79653617537f112e22e031ed2
ghines1254 <ghines1254@gmail.com>
2023-11-24 20:14:49 -0600

    adding branch_id to employee's session

--- HTML/employee-login-page-script.php
+++ HTML/employee-login-page-script.php
@@ -42,12 +42,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 die("Error in executing the statement: " . $stmt->error);
             }
         
-            $stmt->bind_result($idnum, $first_name, $email);
+            $stmt->bind_result($idnum, $first_name, $last_name);
             $stmt->fetch();
         
             $stmt->close();
+            $stmt = $conn->prepare("SELECT POST_OFFICE.branch_id
+                                    FROM EMPLOYEE
+                                    JOIN DEPARTMENT ON EMPLOYEE.dept = DEPARTMENT.dept_id
+                                    JOIN POST_OFFICE ON DEPARTMENT.works_at = POST_OFFICE.branch_id
+                                    WHERE EMPLOYEE.email = ?");
+            $stmt->bind_param("s", $email);
+
+            if (!$stmt->execute()) {
+                die("Error in executing the statement: " . $stmt->error);
+            }
+        
+            $stmt->bind_result($branch_id);
+            $stmt->fetch();
             $conn->close();
-            $emp_info = ['idnum' => $idnum, 'first_name' => $first_name, 'last_name'=> $last_name, 'email' => $email];
+            $emp_info = ['idnum' => $idnum, 'first_name' => $first_name, 'last_name'=> $last_name, 'email' => $email, 'branch_id' => $branch_id];
             $_SESSION['emp_info'] = $emp_info;
             // Redirect to employee portal notifications page (adjust the URL as needed)
             header("Location: employee-portal-notifications-page.php");
